1388056e815c8a6f3fa20c0d47f38a6ea3e0cf48
daichi <test@test.com>
2020-04-27 17:33:05 +0900

    fix

--- utils/line_util.py
+++ utils/line_util.py
@@ -13,20 +13,18 @@ class TextMessageUtil:
             line_bot_api.reply_message(
                 self.event.reply_token, TextSendMessage(text=str(self.event)))
         elif message == 'zoom':
-            target_id = self.event.source.group_id if self.event.source.type == "group" else self.event.source.user_id
-            with open("./config/flex.json") as f:
-                flex_json = json.load(f)
-            flex_message = FlexSendMessage(
-                alt_text='home_room_flex', contents=flex_json["zoom_flex"])
-            line_bot_api.push_message(
-                target_id, messages=flex_message)
-        elif message == 'test21':
-            target_id = self.event.source.group_id if self.event.source.type == "group" else self.event.source.user_id
+            #             target_id = self.event.source.group_id if self.event.source.type == "group" else self.event.source.user_id
             with open("./config/flex.json") as f:
                 flex_json = json.load(f)
             flex_message = FlexSendMessage(
                 alt_text='home_room_flex', contents=flex_json["zoom_flex"])
             line_bot_api.reply_message(
                 self.event.reply_token, messages=flex_message)
+        elif message == 'test21':
+            #            target_id = self.event.source.group_id if self.event.source.type == "group" else self.event.source.user_id
+            with open("./config/flex.json") as f:
+                flex_json = json.load(f)
+            flex_message = FlexSendMessage(
+                alt_text='home_room_flex', contents=flex_json["zoom_flex"])
             line_bot_api.reply_message(
                 self.event.reply_token, messages=flex_message)
