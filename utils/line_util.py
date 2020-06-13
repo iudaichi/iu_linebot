186430c4215d952b336101970d992bd5da075a79
John Doe <johndoe@example.com>
2020-06-13 13:18:27 +0900

    fix && add

--- utils/line_util.py
+++ utils/line_util.py
@@ -13,7 +13,7 @@ class TextMessageUtil:
         if message == 'test':
             line_bot_api.reply_message(
                 self.event.reply_token, TextSendMessage(text=str(self.event)))
-        elif message == 'zoom':
+        elif message.startswith('zoom'):
             #             target_id = self.event.source.group_id if self.event.source.type == "group" else self.event.source.user_id
             with open("./config/flex.json") as f:
                 flex_json = json.load(f)
@@ -21,20 +21,24 @@ class TextMessageUtil:
                 alt_text='home_room_flex', contents=flex_json["zoom_flex"])
             line_bot_api.reply_message(
                 self.event.reply_token, messages=flex_message)
-        elif message == '卒業単位':
+        elif message.startswith('卒業単位'):
             image_message = ImageSendMessage(
                 original_content_url='https://pacific-ocean-18208.herokuapp.com/static/graduation_unit.jpg',
                 preview_image_url='https://pacific-ocean-18208.herokuapp.com/static/graduation_unit.jpg'
             )
             line_bot_api.reply_message(
                 self.event.reply_token, messages=image_message)
-        elif message == 'schedule':
-            now_time = datetime.datetime.now().strftime("%Y/%m/%d")
+        elif message.startswith('schedule'):
+            split_message = message.split(":")
+            if len(split_message) != 1:
+                now_time = split_message[1]
+            else:
+                now_time = datetime.datetime.now().strftime("%Y/%m/%d")
             send_text = f"{now_time}の時間割"
             with open("./excel/schedule.json") as f:
                 schedule_json = json.load(f)
             for v in schedule_json.values():
-                if v['day'] == "2020/06/16":
-                    send_text += f"\n\n{v['day']}({v['day_of_week']})\n{v['time_table']}時間目\n{v['class_name']}\n{v['class_room_number']}\n{v['class_room_password']}\nhttps://zoom.us/j/{v['class_room_number']}?"
+                if v['day'] == now_time:
+                    send_text += f"\n\n{v['time_table']}時間目\n{v['class_name']}\n{v['class_room_number']}\n{v['class_room_password']}\nhttps://zoom.us/j/{v['class_room_number']}?"
             line_bot_api.reply_message(
                 self.event.reply_token, TextSendMessage(text=send_text))
