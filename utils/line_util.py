2023aebff5826ab32d3d4ad64604fcc5ee689af8
John Doe <johndoe@example.com>
2020-06-19 14:18:32 +0900

    test

--- utils/line_util.py
+++ utils/line_util.py
@@ -61,10 +61,10 @@ class TextMessageUtil:
             schedule_json = json.load(f)
         for v in schedule_json.values():
             if v['day'] == now_time:
-                time_table_text = f"{v['time_table']}時間目"
-                class_name_text = f"{v['class_name']}"
-                class_room_password_text = f"pass : {v['class_room_password']}"
-                class_room_url_text = f"https://zoom.us/j/{v['class_room_number']}?"
+                time_table_text = f"{v['time_table'].decode('unicode-escape')}時間目"
+                class_name_text = f"{v['class_name'].decode('unicode-escape')}"
+                class_room_password_text = f"pass : {v['class_room_password'].decode('unicode-escape')}"
+                class_room_url_text = f"https://zoom.us/j/{v['class_room_number'].decode('unicode-escape')}?"
                 template_json["contents"].append({
                     "type": "bubble",
                     "size": "micro",
